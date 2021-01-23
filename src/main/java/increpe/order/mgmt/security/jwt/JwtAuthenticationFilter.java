de38b3d890ef5fe136ec16fa66205ae18d533830
Pankaj Saboo <pankaj.saboo08@gmail.com>
2021-01-23 20:57:13 +0530

    changed packages

--- src/main/java/increpe/order/mgmt/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/increpe/order/mgmt/security/jwt/JwtAuthenticationFilter.java
@@ -1,27 +1,17 @@
 package increpe.order.mgmt.security.jwt;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Service;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import increpe.order.mgmt.security.service.UserDetailsServiceImpl;
+import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static increpe.order.mgmt.security.utils.SecurityConstants.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
