2ccdb9e4e95b45e2a724b7cf0c1cde9ec0b54f6b
Pankaj Saboo <pankaj.saboo08@gmail.com>
2021-02-28 01:34:37 +0530

    code changes

--- build_script.sh
+++ build_script.sh
@@ -2,6 +2,6 @@ echo "Building using "$1"-Profile"
 mvn clean compile -DskipTests
 mvn clean package -DskipTests
 cd target
-java -jar -Dspring.profiles.active=$1 order-management-1.0.0.jar -DskipTests
+nohup java -jar -Dspring.profiles.active=$1 order-management-1.0.0.jar -DskipTests
 cd ..
 $SHELL
