decaaf6e2287080ef79e84a7964c58a6b51dc567
ITPL-Krishna <krishna.kabra@increpe.com>
2021-04-18 04:30:50 +0530

    Added method to get single distributor

--- src/main/java/increpe/order/mgmt/repository/SellerBuyerRelationRepository.java
+++ src/main/java/increpe/order/mgmt/repository/SellerBuyerRelationRepository.java
@@ -2,11 +2,17 @@ package increpe.order.mgmt.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import increpe.order.mgmt.model.SellerBuyerRelation;
 
-public interface SellerBuyerRelationRepository extends CrudRepository<SellerBuyerRelation, Long>{
+public interface SellerBuyerRelationRepository extends PagingAndSortingRepository<SellerBuyerRelation, Long>{
 
 	List<SellerBuyerRelation> findBySellerCompanyId_id(Long sellerCompanyId);
+	
+	List<SellerBuyerRelation> findBySellerCompanyId_id(Long sellerCompanyId, Pageable pageable);
+	
+	SellerBuyerRelation findBySellerCompanyId_idAndBuyerCompanyId_id(Long sellerCompanyId, Long buyerCompanyId);
 }
